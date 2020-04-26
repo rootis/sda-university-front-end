ebecbbc366273147cba28d547d81934637f75023
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-04-26 11:23:58 +0300

    Panel columns

--- src/app/shared/panel/panel.component.ts
+++ src/app/shared/panel/panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelComponent implements OnInit {
 
+  @Input() left: TemplateRef<any>;
+  @Input() right: TemplateRef<any>;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
